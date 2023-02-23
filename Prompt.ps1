704b15b5fa10ac6610bbcad5ed4b0f73035b98ce
LePichu <ishat.prsnl65844@gmail.com>
2023-02-24 00:13:28 +0530

    feat: initial push and prompt script

---
+++ Prompt.ps1
@@ -0,0 +1,48 @@
+Function Prompt {
+    Write-Host ""
+
+    $ARR = @()
+    
+    $MAP = @{
+        "Cargo.toml" = "🦀"
+        "deno.json" = "🦕"
+        "deno.jsonc" = "🦕"
+        "gleam.toml" = "✨"
+        "go.mod" = "🐹"
+        "shard.yaml" = '🔮'
+        "shard.yml" = '🔮'
+        "package.json" = '📦'
+    }
+    
+    ForEach ($KEY in $MAP.Keys) {
+        If (Test-Path "./$KEY") {
+            $ARR += $MAP[$KEY]
+        }
+    }
+
+    $ARR = $ARR | Select-Object -Unique
+
+    $LANGS = ""
+
+    if($ARR.Count -gt 0) {
+        $LANGS += "via "
+
+        if($ARR.Count -eq 1) {
+            $LANGS += $ARR
+        }
+
+        if($ARR.Count -eq 2) {
+            $LANGS += $ARR[0] + " & " + $ARR[1]
+        }
+
+        if($ARR.Count -eq 3) {
+            $LANGS += $ARR[0] + " , " + $ARR[1] + " & " + $ARR[2]
+        }
+    } 
+
+    Write-Host "LePichu@Toaster " -NoNewLine -ForegroundColor DarkCyan
+    Write-Host $LANGS 
+    Write-Host "$>" -NoNewline -ForegroundColor Blue
+
+    " " 
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Pichu Prompt
+A Tiny, Custom, Starship-like prompt for PowerShell running on my systems.
+
+## Usage
+- Save the File somewhere
+- Open your PowerShell Profile Configuration (run `Write-Output $PROFILE` to get path)
+- Run the prompt script via referencing it as a script or copy-paste it into there
+- Feel free to customize however you prefer (optional but recommended)
+
+## License
+MIT License, see [LICENSE](./README.md) for more information.
